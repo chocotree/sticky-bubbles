7b3a3bafa9b4e224f76bf388b7f0fdaedd6d4906
IsTree <IsTree@gmail.com>
2020-05-18 17:59:08 +0800

    fix webpack-dev-server HMR issue by accessing from other device

--- devServer.js
+++ devServer.js
@@ -4,10 +4,11 @@ const WebpackDevServer = require('webpack-dev-server');
 
 // locals
 const webpackConfig = require('./webpack.config');
-const { checkPort } = require('./dev-utils');
+const { checkPort, getLocalExternalIP } = require('./dev-utils');
 
 (async () => {
     let port = 3000;
+    const sockHost = getLocalExternalIP();
 
     await checkPort(port).then(freePort => {
         port = freePort;
@@ -20,12 +21,18 @@ const { checkPort } = require('./dev-utils');
     // webpack-dev-server api example:
     // ⛳ https://github.com/webpack/webpack-dev-server/blob/master/examples/api/simple/server.js
     const compiler = webpack(webpackConfig);
-    const devServerOptions = Object.assign({}, webpackConfig.devServer, {
+    const devServerOptions = {
         contentBase: path.resolve(__dirname, 'build'),
         compress: true,
         hot: true,
         open: true,
-    })
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        // if not specified sockHost, go browsing from other device will find out the socket url is wrong!.
+        // and will show some cors problem in dev console.
+        sockHost,
+    };
     const server = new WebpackDevServer(compiler, devServerOptions);
 
     server.listen(port, '0.0.0.0', () => {
