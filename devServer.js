f78c7fc3e2e5a34d64438370da5dd8c4ab855ef6
IsTree <IsTree@gmail.com>
2020-05-18 14:23:55 +0800

    # add devServer for webpack

---
+++ devServer.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+// locals
+const webpackConfig = require('./webpack.config');
+const { checkPort } = require('./dev-utils');
+
+(async () => {
+    let port = 3000;
+
+    await checkPort(port).then(freePort => {
+        port = freePort;
+    })
+        .catch(err => {
+            console.log(err.message);
+            process.exit();
+        })
+
+    // webpack-dev-server api example:
+    // ⛳ https://github.com/webpack/webpack-dev-server/blob/master/examples/api/simple/server.js
+    const compiler = webpack(webpackConfig);
+    const devServerOptions = Object.assign({}, webpackConfig.devServer, {
+        contentBase: path.resolve(__dirname, 'build'),
+        compress: true,
+        hot: true,
+        open: true,
+    })
+    const server = new WebpackDevServer(compiler, devServerOptions);
+
+    server.listen(port, '127.0.0.1', () => {
+        console.log('starting server~');
+    })
+})();
